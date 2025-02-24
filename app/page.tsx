0a29d82f4fbeae724a8238504aa81d721178361e
Eman Tariq <emantariq.1004@gmail.com>
2025-02-24 19:53:51 +0500

    building posts

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,25 @@
 import Header from "@/components/Header";
+import { client } from "../sanity/lib/client";
+import { urlFor } from "../sanity/lib/image";
+import type { Post } from "@/types";
+import Link from "next/link";
 
-export default function Home() {
+const query = `* [_type == "post"]{
+_id,  
+  title,
+  slug,
+  author -> {
+name,
+    image
+  },
+  description,
+  mainImage,
+  slug
+}`;
+
+export default async function Home() {
+  const posts: Post[] = await client.fetch(query);
+
   return (
     <div className="max-w-7xl mx-auto">
       <Header />
@@ -24,6 +43,41 @@ export default function Home() {
           src="https://i.pinimg.com/236x/1c/bf/66/1cbf664cffad0285718470acb9c23106.jpg"
           alt="M"
         />
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 md:p-6">
+        {posts.map((posts) => (
+          <Link
+            key={posts._id}
+            href={`/post/${posts.slug.current}`}
+            className=""
+          >
+            <div className="group">
+              <img
+                className="h-60 w-full object-cover group-hover:scale-105"
+                src={
+                  posts.mainImage
+                    ? urlFor(posts.mainImage).url()
+                    : "/fallback.jpg"
+                }
+                alt=""
+              />
+              <div className="flex justify-between p-5 bg-white">
+                <div>
+                  <p>{posts.title}</p>
+                  <p>
+                    {posts.description} by {posts.author.name}
+                  </p>
+                </div>
+                <img
+                  className="h-12 w-12 rounded-full"
+                  src={urlFor(posts.author.image).url()!}
+                  alt=""
+                />
+              </div>
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   );
